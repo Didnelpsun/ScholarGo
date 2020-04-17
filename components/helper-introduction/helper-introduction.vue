600e7782a7591ac437727ab43b33df05185793b8
pineleaf <1448652572@qq.com>
2020-04-17 10:04:25 +0800

    简单写了帮助者介绍组件，但是仍然存在传参问题，另外一些描述性信息也需要注意

---
+++ components/helper-introduction/helper-introduction.vue
@@ -0,0 +1,28 @@
+<template>
+	<view>
+		<uni-card :extra="area" :title="name" mode="style" :is-shadow="true" :thumbnail="photo"
+		 :note="grade">
+			{{notes}}
+		</uni-card>
+	</view>
+</template>
+
+<script>
+	import uniCard from '@/components/uni-card/uni-card.vue';
+	export default {
+		name: 'helperIntroduction',
+		data () {
+			return{};
+		},
+		props: [
+			'name',
+			'photo',
+			'area',
+			'grade',
+			'notes'
+			],
+		components: {
+			uniCard
+		}
+	}
+</script>
