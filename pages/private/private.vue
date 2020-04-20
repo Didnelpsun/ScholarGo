d64362fe14b0a84285e68348facf87ef0a2b0232
Didnelpsun <2675350965@qq.com>
2020-04-21 01:10:03 +0800

    Update private.vue

--- pages/private/private.vue
+++ pages/private/private.vue
@@ -1,6 +1,63 @@
 <template>
 	<view class="private">
-		
+		<text class="text">欢迎光临本app，请您仔细阅读以下条款，如果您对本协议的任何条款表示异议，您可以选择不使用本app；进入本app则意味着您将同意遵守本协议下全部规定，并完全服从于app开发者的统一管理。 </text>
+		<text class="title">第一章 总则</text>
+		<text class="text">第1条 本app是app开发者向用户提供的信息存储空间，为用户提供讨论、交流的平台。</text>
+		<text class="text">第2条 本app所有权、经营权、管理权均属app开发者。</text>
+		<text class="text">第3条 本协议最终解释权归属app开发者。</text>
+		<text class="title">第二章 app用户</text>
+		<text class="text">第4条 凡是注册用户和浏览用户均为app用户（以下统称"用户"）。</text>
+		<text class="text">第5条 用户的个人信息受到保护，不接受任何个人或单位的查询。国家机关依法查询除外，用户的个人设置公开除外。</text>
+		<text class="text">第6条 用户享有言论自由的权利。</text>
+		<text class="text">第7条 用户的言行不得违反《计算机信息网络国际联网安全保护管理办法》、《互联网信息服务管理办法》、《互联网电子公告服务管理规定》、《维护互联网安全的决定》、《互联网新闻信息服务管理规定》等相关法律规定，不得在本app发布、传播或以其它方式传送含有下列内容之一的信息：</text>
+		<text class="text">1.反对宪法所确定的基本原则的；</text>
+		<text class="text">2.危害国家安全，泄露国家秘密，颠覆国家政权，破坏国家统一的；</text>
+		<text class="text">3.损害国家荣誉和利益的；</text>
+		<text class="text">4.煽动民族仇恨、民族歧视、破坏民族团结的；</text>
+		<text class="text">5.破坏国家宗教政策，宣扬邪教和封建迷信的；</text>
+		<text class="text">6.散布谣言，扰乱社会秩序，破坏社会稳定的；</text>
+		<text class="text">7.散布淫秽、色情、赌博、暴力、凶杀、恐怖或者教唆犯罪的；</text>
+		<text class="text">8.侮辱或者诽谤他人，侵害他人合法权利的；</text>
+		<text class="text">9.煽动非法集会、结社、游行、示威、聚众扰乱社会秩序的；</text>
+		<text class="text">10.以非法民间组织名义活动的；</text>
+		<text class="text">11.含有虚假、有害、胁迫、侵害他人隐私、骚扰、侵害、中伤、粗俗、猥亵、或其它道德上令人反感的内容；</text>
+		<text class="text">12.含有中国法律、法规、规章、条例以及任何具有法律效力之规范所限制或禁止的其它内容的。</text>
+		<text class="text">第8条 用户不得在app内发布任何形式的广告，包括但不限于：</text>
+		<text class="text">1.发布带有任何有联系方式的网络创业、网络兼职、刷Q币以及非真实性中奖信息；</text>
+		<text class="text">2.内容包含带有任何联系方式的学历职称代办，专业代做试题，作业，论文等题目以及售卖考试答案等信息；</text>
+		<text class="text">3.发布任何带有联系方式的银行卡代办、买卖发票、非法彩票等信息；</text>
+		<text class="text">4.含有联系方式的黑客、收费删帖、办证刻章等违法信息；</text>
+		<text class="text">5.非官方认证的发布任何带有联系方式的医院、美容、药品、祛斑、医生专家和整容等信息。</text>
+		<text class="text">第9条 用户应承担一切因其个人的行为而直接或间接导致的民事或刑事法律责任，因用户行为给app开发者造成损失的，用户应负责赔偿。</text>
+		<text class="text">第10条 app开发者拥有对违反本app规则的用户进行处理的权力，直至禁止其在本app内发布信息。</text>
+		<text class="text">第11条 禁止任何使用非法软件进行灌水、违规发布内容的行为。</text>
+		<text class="text">第12条 任何用户发现app内容涉嫌侮辱或者诽谤他人、侵害他人合法权益的，有权按app投诉规则进行投诉。</text>
+		<text class="text">第13条 为了能够给广大用户提供一个优质的交流平台，同时使app能够良性、健康的发展，将对涉及反动、色情的头像、签名档和发布不良内容的用户，进行严厉处理。一经发现此类行为，将给予永久封禁ID并清空所有发言的处罚。</text>
+		<text class="title">第三章 权利声明</text>
+		<text class="text">第14条 app内容由用户创建，发布的内容或者文章仅代表作者观点，与app开发者无关。对于用户言论的真实性引发的全部责任，由用户自行承担，app开发者不承担任何责任。</text>
+		<text class="text">第15条 用户之间因使用app而产生或可能产生的任何纠纷和/或损失，由用户自行解决并承担相应的责任，与app开发者无关。</text>
+		<text class="text">第16条 用户在app发表的作品，app开发者有权转载或引用，无需通知用户。</text>
+		<text class="text">第17条 用户在任何时间段在app发表的任何内容（包括自拍）的著作财产权，用户许可app开发者在全世界范围内免费地、永久性地、不可撤销地、可分许可地和非独家地使用的权利，包括但不限于：复制权、发行权、出租权、展览权、表演权、放映权、广播权、信息网络传播权、摄制权、改编权、翻译权、汇编权以及《著作权法》规定的由著作权人享有的其他著作财产权利。并且，用户许可app开发者有权利就任何主体侵权而单独提起诉讼，并获得全部赔偿。</text>
+		<text class="title">第四章 处罚规则</text>
+		<text class="text">第18条 app开发者郑重提醒用户，若出现下列情况任意一种或几种，将承担包括被关闭全部或者部分权限、被暂停或被删除其帐号的后果，情节严重的，还将承担相应的法律责任。</text>
+		<text class="text">1.使用不雅或不恰当ID和昵称；</text>
+		<text class="text">2.发表含有猥亵、色情、人身攻击和反政府言论等非法或侵权言论的；</text>
+		<text class="text">3.从事非法商业活动；</text>
+		<text class="text">4.模仿管理人员ID或者他人ID，用以假冒管理人员或破坏管理人员形象；</text>
+		<text class="text">5.使用非法软件进行刷单、刷屏的行为。</text>
+		<text class="text">6.其他app开发者认为不恰当的情况。</text>
+		<text class="text">第19条 凡文章出现以下情况之一，app管理人员有权不提前通知作者直接删除，并依照有关规定作相应处罚。情节严重者，app管理人员有权对其做出关闭部分权限、暂停直至删除其帐号。 </text>
+		<text class="text">1.发表含有本协议第7条禁止发布、传播内容的文章；</text>
+		<text class="text">2.发表不符合版面主题，或者无内容的灌水文章；</text>
+		<text class="text">3.同一文章多次出现的；</text>
+		<text class="text">4.违反本协议第8条的规定，发布广告的；</text>
+		<text class="text">5.文章内容或个人签名会包含有严重影响用户浏览的内容或格式的；</text><text class="text"></text>
+		<text class="text">6.其他app开发者认为不恰当的情况。</text>
+		<text class="title">第五章 附则</text>
+		<text class="text">第20条 关于打击网络谣言的司法解释：《最高人民法院、最高人民检察院关于办理利用信息网络实施诽谤等刑事案件适用法律若干问题的解释》已于2013年9月5日由最高人民法院审判委员会第1589次会议、2013年9月2日由最高人民检察院第十二届检察委员会第9次会议通过，现予公布，自2013年9月10日起施行。</text>
+		<text class="text">第21条 所有用户发表的文章而引起的法律纠纷，与app开发者无关。 </text>
+		<text class="text">第22条 app如因系统维护或升级等而需暂停服务时，将事先公告。若因硬件故障或其它不可抗力而导致暂停服务，于暂停服务期间造成的一切不便与损失，不负任何责任。 由于app的调整导致信息丢失和/或其他结果的，app开发者不承担任何责任。</text>
+		<text class="text">第23条 本协议未涉及的问题参见国家有关法律法规，当本协议与国家法律法规冲突时，以国家法律法规为准。</text>
 	</view>
 </template>
 
@@ -17,8 +74,20 @@
 <style lang="scss" scoped>
 	.private{
 		background-color: $uni-bg-color-grey;
-		color: $uni-text-color;
 		font-size: $uni-font-size-base;
-		height: 100vh;
+		padding: 2 * $space;
+		text-indent: 2em;
+		display: flex;
+		flex-direction: column;
+	}
+	.title{
+		font-size: $uni-font-size-lg;
+		color: $uni-text-color;
+	}
+	.text{
+		color: $uni-text-color-grey;
+	}
+	text{
+		margin: 5px 0 5px;
 	}
 </style>
