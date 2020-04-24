063f95ed61f729a55a107f4f02df394f421574c3
Didnelpsun <2675350965@qq.com>
2020-04-24 23:40:08 +0800

    对于用户主页面的更新

--- pages/user_main/user_main.vue
+++ pages/user_main/user_main.vue
@@ -1,19 +1,41 @@
 <template>
-	<view>
-		
+	<view class="user">
+		<view class="back">返回用户中心</view>
+		<click-card
+		title="个人主页"
+		arrow="true"
+		/>
+		<click-card
+		title="手机号码"
+		:subtitle="telephone"
+		arrow="true"
+		/>
 	</view>
 </template>
 
 <script>
+	import clickCard from '@/components/click-card/click-card.vue'
 	export default {
 		data() {
 			return {
-				
+				telephone:'136454546',
+				class1: "background-color:red"
 			};
+		},
+		components:{
+			clickCard
 		}
 	}
 </script>
 
-<style lang="scss">
-
+<style lang="scss" scoped>
+	.user{
+		background-color: $uni-bg-color-grey;
+		font-size: $uni-font-size-base;
+		height: 100vh;
+	}
+	.back{
+		
+	}
+	
 </style>
