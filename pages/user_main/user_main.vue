6dcb386d6a969a556fedbda14565d8e90bf50425
Didnelpsun <2675350965@qq.com>
2020-04-26 16:40:54 +0800

    Update user_main.vue

--- pages/user_main/user_main.vue
+++ pages/user_main/user_main.vue
@@ -1,6 +1,6 @@
 <template>
 	<view class="user">
-		<view class="back">返回用户中心</view>
+		<view class="back" @click="swithTab()">返回用户中心</view>
 		<click-card
 		title="个人主页"
 		arrow="true"
@@ -11,13 +11,15 @@
 		arrow="true"
 		/>
 		<click-card
-		title="手机号码"
-		:subtitle="telephone"
+		title="我的二维码"
 		arrow="true"
 		/>
 		<click-card
-		title="手机号码"
-		:subtitle="telephone"
+		title="邀请有礼"
+		arrow="true"
+		/>
+		<click-card
+		title="赞助我们"
 		arrow="true"
 		/>
 	</view>
@@ -34,6 +36,17 @@
 		},
 		components:{
 			clickCard
+		},
+		methods:{
+			swithTab(){
+				uni.switchTab({
+					url:'/pages/user/user',
+					fail() {
+						uni.navigateBack({
+						})
+					}
+				})
+			}
 		}
 	}
 </script>
@@ -45,7 +58,13 @@
 		height: 100vh;
 	}
 	.back{
-		
+		background-color: #007AFF;
+		height: 40px;
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		color: $uni-text-color-inverse;
+		padding: 2 * $space;
+		font-size: $uni-font-size-lg;
 	}
-	
 </style>
